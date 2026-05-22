5762c89282a15fa088e162db58c3412d0be01749
DemenDark <volodya.shirko@gamil.com>
2026-05-22 09:15:59 +0300

    test1.4

--- PharmacyApp/urls.py
+++ PharmacyApp/urls.py
@@ -1,5 +1,5 @@
 from django.conf import settings
-from django.conf.urls.static import static
+from django.views.static import serve
 from django.contrib import admin
 from django.urls import include, re_path
 from django.contrib.auth import views as auth_views
@@ -11,5 +11,6 @@ urlpatterns = [
     re_path(r'^', include('pharmacy.urls')),
     re_path(r'^', include('info.urls')),
 ]
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
+urlpatterns += [
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+]
