a47917c48d74cb615841234ee79368d6ddcf2d9c
DemenDark <volodya.shirko@gamil.com>
2026-05-22 06:49:47 +0300

    change setting1/2

--- PharmacyApp/urls.py
+++ PharmacyApp/urls.py
@@ -11,6 +11,5 @@ urlpatterns = [
     re_path(r'^', include('pharmacy.urls')),
     re_path(r'^', include('info.urls')),
 ]
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
